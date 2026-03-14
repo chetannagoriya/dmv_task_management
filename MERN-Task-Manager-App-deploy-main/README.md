b60eee43e115378403684153375f381180bd35c0
Chetan Nagoriya <152314423+chetannagoriya@users.noreply.github.com>
2026-03-14 19:52:24 +0530

    Delete MERN-Task-Manager-App-deploy-main directory

--- MERN-Task-Manager-App-deploy-main/README.md
+++
@@ -1,1 +0,0 @@
-"# MERN-Task-Manager-App-deploy" 
--- MERN-Task-Manager-App-deploy-main/frontend/src/utils.js
+++
@@ -1,7 +0,0 @@
-import { toast } from 'react-toastify';
-
-export const notify = (message, type) => {
-    toast[type](message);
-}
-
-export const API_URL = 'https://mern-task-manager-app-deploy-api.vercel.app';
